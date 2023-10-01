221c2d6df4f049cab7737aec02ee11945dc53fdf
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-10-01 19:46:44 +0300

    [nix] Make a derivation
    Change-Id: Ie9ccc75ed86b72cc1a61e15ba9274803379e51aa

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    # TODO(Shvedov) Required unstable channel fot rust >= 1.70
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
@@ -12,20 +14,43 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        buildInputs = with pkgs; [
+          gtk4
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          wrapGAppsHook4
+        ];
       in
-      {
-
+      rec {
+        packages = rec {
+          ide-manager = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "ide-manager";
+            version = "0.2";
+            inherit nativeBuildInputs buildInputs;
+            src = with builtins; path {
+              filter = (path: type:
+                let
+                  bn = baseNameOf path;
+                in
+                bn != "flake.nix" && bn != "flake.lock"
+              );
+              path = self;
+            };
+            cargoLock.lockFile = "${self}/Cargo.lock";
+          };
+          default = ide-manager;
+        };
         devShells.default = with pkgs; mkShellNoCC {
           buildInputs = [
             rust-bin.stable.latest.default
             probe-run
             rust-analyzer
-            pkg-config
-            gtk4
-          ];
+          ] ++ buildInputs ++ nativeBuildInputs;
           shellHook = ''
             export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
           '';
         };
-      });
+      }
+    );
 }
